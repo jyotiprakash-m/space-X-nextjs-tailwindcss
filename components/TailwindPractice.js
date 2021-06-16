bff4a5190a47a8cc63a075283e0d5502340fbe88
jyotiprakash-m <teachmeonline365@gmail.com>
2021-06-16 20:20:13 +0530

    practice tailwindcss

---
+++ components/TailwindPractice.js
@@ -0,0 +1,153 @@
+import React from 'react'
+
+function TailwindPractice() {
+    return (
+        <div>
+            {/* <div className="bg-blue-300 p-4 mt-4 mx-4 font-sans text-xl text-justify text-blue-900">
+
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+            <div className="bg-red-300 p-4 mt-4 mx-4 font-sans text-xl text-justify text-red-900 tracking-tight leading-loose">
+
+                <span className="italic font-semibold underline capitalize font-"> Lorem Ipsum </span> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+            <div className="bg-blue-200 m-4 p-4">
+                <h1 className="text-gray-900 capitalize font-semibold text-4xl text-center">This is basically a title heading</h1>
+                <p className="text-justify tracking-wide leading-loose mt-4">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                </p>
+                <div className="bg-blue-900 m-12 p-4">
+                    <p className="italic text-xl text-gray-100">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                    </p>
+
+                    <p className="text-right mt-3 font-semibold text-2xl text-gray-100">- Jyoti Prakash,<span>Smart Developer</span></p>
+                </div>
+
+                <button className="bg-blue-800 text-gray-100 uppercase px-12 py-4">enroll now</button>
+            </div>
+
+            <div className="bg-gray-600 w-full h-1"></div>
+
+
+            <div className="w-64 h-64 border-4 border-green-400 m-4 rounded-full">
+
+            </div>
+            <div className="w-64 h-64 border-4 border-green-400 m-4 rounded-tl-2xl">
+
+            </div> */}
+
+            {/* Buttons */}
+            {/* <div className="m-4">
+                <button className="bg-blue-500 rounded-xl text-gray-100 text-xl border border-l-4 border-red-500 px-8 py-2">Submit</button>
+            </div>
+            <div className="m-4">
+                <button className="bg-indigo-200 border-b  border-indigo-500 text-indigo-800 py-2 px-8">Save</button>
+            </div>
+            <div className="m-4">
+                <button className=" border border-red-600 rounded-md text-red-600 px-8 py-2 outline hover:text-yellow-300 hover:border-indigo-800">Save</button>
+            </div>
+            <div className="m-4">
+                <button className="font-serif uppercase border border-t-4 border-green-700 rounded-xl px-8 py-1">Save</button>
+            </div> */}
+
+            {/* display */}
+
+            {/* <div>Jyotiprakash<div className="bg-purple-500 w-min text-3xl">Mohanta</div></div> */}
+
+            {/* Flex-box in Tailwind css */}
+
+            {/* <div className="flex flex-wrap gap-3  justify-center h-screen mx-8">
+                <div className="w-32 h-32 bg-green-600 flex items-center justify-center">1</div>
+                <div className="w-32 h-32 bg-blue-600 flex items-center justify-center">2</div>
+                <div className="w-32 h-32 bg-red-600 flex items-center justify-center">3</div>
+                <div className="w-32 h-32 bg-green-600 flex items-center justify-center">1</div>
+                <div className="w-32 h-32 bg-blue-600 flex items-center justify-center">2</div>
+                <div className="w-32 h-32 bg-red-600 flex items-center justify-center">3</div>
+                <div className="w-32 h-32 bg-green-600 flex items-center justify-center">1</div>
+                <div className="w-32 h-32 bg-blue-600 flex items-center justify-center">2</div>
+                <div className="w-32 h-32 bg-red-600 flex items-center justify-center">3</div>
+                <div className="w-32 h-32 bg-green-600 flex items-center justify-center">1</div>
+                <div className="w-32 h-32 bg-blue-600 flex items-center justify-center">2</div>
+                <div className="w-32 h-32 bg-red-600 flex items-center justify-center">3</div>
+                <div className="w-32 h-32 bg-green-600 flex items-center justify-center">1</div>
+                <div className="w-32 h-32 bg-blue-600 flex items-center justify-center">2</div>
+                <div className="w-32 h-32 bg-red-600 flex items-center justify-center">3</div>
+                <div className="w-32 h-32 bg-green-600 flex items-center justify-center">1</div>
+                <div className="w-32 h-32 bg-blue-600 flex items-center justify-center">2</div>
+                <div className="w-32 h-32 bg-red-600 flex items-center justify-center">3</div>
+            </div> */}
+
+            {/* Flex box challenge */}
+
+            {/* <div className="flex gap-4 justify-around flex-wrap items-center">
+                <div className="bg-green-100 w-full h-32 font-mono text-2xl text-red-500 flex justify-center items-center">A</div>
+                <div className="bg-green-200 w-1/4 h-32 font-mono text-2xl text-red-500 flex justify-center items-center">B</div>
+                <div className="bg-green-300 w-1/4 h-32 font-mono text-2xl text-red-500 flex justify-center items-center">C</div>
+                <div className="bg-green-400 w-1/4 h-32 font-mono text-2xl text-red-500 flex justify-center items-center">D</div>
+                <div className="bg-green-500 w-1/4 h-32 font-mono text-2xl text-red-500 flex justify-center items-center">E</div>
+                <div className="bg-green-600 w-1/4 h-32 font-mono text-2xl text-red-500 flex justify-center items-center">F</div>
+                <div className="bg-green-700 w-1/4 h-32 font-mono text-2xl text-red-500 flex justify-center items-center">G</div>
+                <div className="bg-green-800 w-1/4 h-32 font-mono text-2xl text-red-500 flex justify-center items-center">H</div>
+                <div className="bg-green-900 w-1/4 h-32 font-mono text-2xl text-red-500 flex justify-center items-center">I</div>
+            </div> */}
+
+
+            {/* Make the page responsive -- modifies */}
+
+
+            {/* Task create a input component */}
+            {/* <div className="h-screen bg-gray-300 p-4 flex items-center justify-center">
+
+                <div className="h-1/2 w-full md:w-1/2 lg:w-1/3 bg-gray-100 border-2 rounded-md border-gray-900 flex justify-center items-center">
+                    <div className="flex flex-col items-center w-full">
+
+                        <label for="input_field" className="capitalize text-xl font-bold text-gray-800 select-none">full name</label>
+                        <input id="input_field" type="text" placeholder="Enter your full name" className="w-2/3 my-4 h-10 p-2 border-2 rounded border-gray-600 text-gray-700 focus:bg-green-100 placeholder-gray-600" />
+                        <button className="rounded-xl bg-gray-700 text-gray-100 px-8 py-2 hover:bg-gray-800">Save</button>
+                    </div>
+
+                </div>
+            </div> */}
+
+
+            {/* Task -Create employee card */}
+            <div className="h-screen bg-gray-500 flex items-center justify-center p-4">
+                <div className="h-full w-full md:w-1/2 lg:w-1/3 bg-gray-100 relative">
+                    <div className="rounded">
+                        <div className="bg-gray-300 p-4">
+                            <h1 className="text-gray-900">The title of the card here</h1>
+                        </div>
+                        <div className="flex justify-between p-4">
+                            <button className="bg-yellow-600 text-gray-100  px-2 rounded-full uppercase">under review</button>
+                            <p>May 14,1988</p>
+                        </div>
+                        <div className="m-4 bg-gray-300 rounded-md p-4">
+                            <p>Here is a short comment about this employee</p>
+                        </div>
+                        <div className="absolute bottom-0 p-4 bg-gray-300 w-full">
+                            <p className="uppercase text-gray-600">employee</p>
+                            <div className="mt-2 flex gap-3">
+                                <div className="bg-blue-700 w-12 h-12 rounded-full flex items-center justify-center">
+                                    <p className="uppercase text-gray-100 font-bold">vg</p>
+                                </div>
+                                <div>
+                                    <h1 className="font-bold text-black capitalize">victor gonzalez</h1>
+                                    <p>instructor</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default TailwindPractice
