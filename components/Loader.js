0226db108f86891630241851a2356d60e1537371
jyotiprakash-m <teachmeonline365@gmail.com>
2021-06-18 12:28:00 +0530

    loader page done

---
+++ components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Loader() {
+    return (
+        <div className="w-full h-screen flex justify-center">
+            <img className="" src="./loader2.gif" />
+
+        </div>
+    )
+}
+
+export default Loader
